7b490c2b86bd8203a42f34671d0f2ccdd0d2a6e3
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2025-10-04 15:08:21 +0000

    init settings in application.properties

---
+++ src/main/java/com/example/javaredisbackend/JavaRedisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaredisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRedisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRedisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaredisbackend/JavaRedisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaredisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRedisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
